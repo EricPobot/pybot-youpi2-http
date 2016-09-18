cd99557a3150a65bd5e3377207e83f0449169a32
Eric Pascual <eric@pobot.org>
2016-09-18 16:51:33 +0200

    added motors control

--- src/pybot/youpi2/http/apps/api.py
+++ src/pybot/youpi2/http/apps/api.py
@@ -18,9 +18,12 @@ class RestAPIApp(YoupiBottleApp):
         self.route('/settings', 'GET', callback=self.get_settings)
         self.route('/pose', 'GET', callback=self.get_pose)
         self.route('/pose', 'PUT', callback=self.set_pose)
-        self.route('/position/<joint>', 'GET', callback=self.get_joint_position)
-        self.route('/position/<joint>', 'PUT', callback=self.set_joint_position)
+        self.route('/move', 'PUT', callback=self.move)
+        self.route('/joint/<joint>', 'GET', callback=self.get_joint_position)
+        self.route('/joint/<joint>', 'PUT', callback=self.set_joint_position)
         self.route('/gripper', 'PUT', callback=self.set_gripper_state)
+        self.route('/motors', 'GET', callback=self.get_motor_positions)
+        self.route('/motors', 'PUT', callback=self.set_motor_positions)
         self.route('/home', 'PUT', callback=self.go_home)
         self.route('/hi_z', 'PUT', callback=self.hi_z)
         self.route('/calibrate', 'PUT', callback=self.calibrate)
@@ -29,16 +32,22 @@ class RestAPIApp(YoupiBottleApp):
         self.log_error(msg)
         return HTTPError(status, msg)
 
+    @staticmethod
+    def _pythonize_values_dict(d):
+        return {
+            k: float(v) for k, v in d.iteritems()
+        }
+
     def get_version(self):
         return {'version': version}
 
     def get_settings(self):
         return {
-            YoupiArm.MOTOR_NAMES[j]: s for j, s in enumerate(self.arm.get_settings())
+            j: self._pythonize_values_dict(s) for j, s in self.arm.get_settings().iteritems()
         }
 
     def get_pose(self):
-        return {YoupiArm.MOTOR_NAMES[j]: float(p) for j, p in enumerate(self.arm.get_current_positions())}
+        return self._pythonize_values_dict(self.arm.get_joint_positions())
 
     def set_pose(self):
         try:
@@ -46,7 +55,21 @@ class RestAPIApp(YoupiBottleApp):
         except ValueError as e:
             return self._http_error(404, 'joint name not found (%s)' % str(e).split()[0])
 
-        self.arm.goto(pose, True)
+        if pose:
+            self.arm.goto(pose, True)
+        else:
+            return HTTPError(400, 'missing pose settings')
+
+    def move(self):
+        try:
+            angles = {YoupiArm.MOTOR_NAMES.index(j): float(a) for j, a in request.query.iteritems()}
+        except ValueError as e:
+            return self._http_error(404, 'joint name not found (%s)' % str(e).split()[0])
+
+        if angles:
+            self.arm.move(angles, True)
+        else:
+            return HTTPError(400, 'missing angles settings')
 
     def get_joint_position(self, joint):
         try:
@@ -58,7 +81,7 @@ class RestAPIApp(YoupiBottleApp):
                 return self._http_error(404, 'joint name not found (%s)' % joint)
 
         try:
-            position = float(self.arm.get_current_positions()[joint_id])
+            position = float(self.arm.get_joint_positions()[joint_id])
         except IndexError:
             return self._http_error(404, 'joint id not found (%d)' % joint_id)
         else:
@@ -73,7 +96,25 @@ class RestAPIApp(YoupiBottleApp):
             except ValueError:
                 return self._http_error(404, 'joint name not found (%s)' % joint)
 
-        self.arm.goto({joint_id: float(request.query.pos)}, True)
+        angle = request.query.angle
+        if angle:
+            self.arm.goto({joint_id: float(angle)}, True)
+        else:
+            return HTTPError(400, 'missing angle argument')
+
+    def get_motor_positions(self):
+        return self._pythonize_values_dict(self.arm.get_motor_positions())
+
+    def set_motor_positions(self):
+        try:
+            positions = {YoupiArm.MOTOR_NAMES.index(j): float(a) for j, a in request.query.iteritems()}
+        except ValueError as e:
+            return self._http_error(404, 'motor name not found (%s)' % str(e).split()[0])
+
+        if positions:
+            self.arm.motor_goto(positions, True)
+        else:
+            return HTTPError(400, 'missing motors settings')
 
     def go_home(self):
         self.arm.go_home([m for m in YoupiArm.MOTORS_ALL if m != YoupiArm.MOTOR_GRIPPER], True)
