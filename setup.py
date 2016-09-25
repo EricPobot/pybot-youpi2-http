1a80d973641881294da2acc49931203398ea5a75
Eric Pascual <eric@pobot.org>
2016-09-25 14:59:46 +0200

    dependencies updated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license='',
     author='Eric Pascual',
     author_email='eric@pobot.org',
-    install_requires=['pybot-youpi2>=0.23', 'pybot-lcd-fuse>=0.20.1', 'bottle>=0.12.9'],
+    install_requires=['pybot-youpi2-app', 'bottle>=0.12.9'],
     download_url='https://github.com/Pobot/PyBot',
     description='Youpi2 REST API server',
     entry_points={
