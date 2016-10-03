9fd4923f7373b531bf6c6f78552c23007178b0ef
Eric Pascual <eric@pobot.org>
2016-10-03 02:31:40 +0200

    restart-srvr task added

--- fabfile.py
+++ fabfile.py
@@ -2,3 +2,8 @@ from pybot.fabtasks import *
 from fabric.state import output
 
 output.output = False
+
+
+@task(alias="restart-srvr")
+def restart_http_doc():
+    sudo('systemctl restart youpi2-http-doc.service')
